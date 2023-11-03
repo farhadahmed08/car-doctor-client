c4242a7ef2067baaba922b3d08d58cac0bea72e5
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-04 03:18:20 +0800

    update and delete done

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -17,11 +17,54 @@ const Bookings = () => {
         .then(data=>setBookings(data))
     },[url])
 
+    const handleDelete =(id)=>{
+        const proceed = confirm('Are you sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`,{
+                method:"DELETE"
+            })
+            .then(res=>res.json())
+            .then(data =>{
+                console.log(data);
+                if (data.deletedCount>0) {
+                    alert('Deleted Successful')
+                    const remaining = bookings.filter(booking=>booking._id !== id)
+                    setBookings(remaining)
+                }
+            })
+        }
+      }
+
+      const handleBookingConfirm = id =>{
+        fetch(`http://localhost:5000/bookings/${id}`,{
+            method:'PATCH',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({status:'Confirm'})
+        })
+        .then(res=>res.json())
+        .then(data =>{
+            console.log(data);
+            if (data.acknowledged === true) {
+                //update state
+                const remaining = bookings.filter(booking=> booking._id !== id);
+                const updated = bookings.find(booking=>booking._id ===id);
+                updated.status ='confirm'
+                const newBookings = [updated,...remaining];
+                setBookings(newBookings)
+
+
+
+            }
+        })
+      }
+
 
 
     return (
         <div>
-            <h2>Your Booking:{bookings.length}</h2>
+            <h2 className="text-5xl">Your Booking:{bookings.length}</h2>
             <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
@@ -36,12 +79,13 @@ const Bookings = () => {
         <th>Service</th>
         <th>Date</th>
         <th>Price</th>
+        <th>Status</th>
       </tr>
     </thead>
     <tbody>
       {/* row 1 */}
       {
-        bookings?.map(booking=><BookingRow key={booking._id} booking={booking}></BookingRow>)
+        bookings?.map(booking=><BookingRow key={booking._id} booking={booking} handleDelete={handleDelete} handleBookingConfirm={handleBookingConfirm}></BookingRow>)
       }
    
     </tbody>
